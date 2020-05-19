e182a40d838785d92d593e980aa28babef045154
Yamuna <yamuna@Yamunas-MacBook-Pro.local>
2020-05-19 16:56:12 -0700

    first commit

---
+++ src/main/java/com/git/demo/Test.java
@@ -0,0 +1,14 @@
+package com.git.demo;
+
+public class Test {
+
+   public static void main(String[] args) {
+    method1();
+       
+} 
+    
+    public static void method1() {
+        System.out.println("This is method 1");
+    }
+    
+}
